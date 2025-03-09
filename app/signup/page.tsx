a5ac4874c9bcdaea70f61811227dcf91dc23d8c7
Himani <himani1@tothenew.com>
2025-03-09 12:48:23 +0530

    signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import styles from "@/styles/signup.module.css";
+import Image from "next/image";
+
+const SignupPage = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.signup_box}>
+        <h2>Sign Up</h2>
+        <p>
+          Already a member? <a href="#">Login.</a>
+        </p>
+        <form className={styles.signup_form}>
+            {/* role */}
+            <label>Role</label>
+          <div className={styles.input_box}>
+            <span className="icon">
+                <Image
+                    src={'Customer.svg'}
+                    width={15}
+                    height={15}
+                    alt='customer'
+                />
+            </span>
+            <input type="email" placeholder="Select role" />
+          </div>
+            {/* name */}
+            <label>Name</label>
+          <div className={styles.input_box}>
+            <span className="icon">
+            <Image
+                    src={'ID.svg'}
+                    width={15}
+                    height={15}
+                    alt='customer'
+                />
+            </span>
+            <input type="email" placeholder="Enter your name" />
+          </div>
+            {/* phone */}
+            <label>Phone</label>
+          <div className={styles.input_box}>
+            <span className="icon">
+            <Image
+                    src={'Phone.svg'}
+                    width={15}
+                    height={15}
+                    alt='customer'
+                />
+            </span>
+            <input type="email" placeholder="Enter your phone number" />
+          </div>
+            {/* email */}
+            <label>Email</label>
+          <div className={styles.input_box}>
+            <span className="icon">
+            <Image
+                    src={'At.svg'}
+                    width={15}
+                    height={15}
+                    alt='customer'
+                />
+            </span>
+            <input type="email" placeholder="Enter your email address" />
+          </div>
+            {/* password */}
+            <label>Password</label>
+          <div className={styles.input_box} id={styles.password_input}>
+            <div className={styles.p}>
+              <span className="icon">
+                <Image src={"Lock.svg"} alt="lock" height={15} width={15} />
+              </span>
+              <input type="password" placeholder="••••••••" />
+            </div>
+            <div>
+              <a>
+                <Image src={"Eye.svg"} alt="lock" height={15} width={15} />
+              </a>
+            </div>
+          </div>
+          <button className={styles.submit_btn}>Submit</button>
+          <button className={styles.reset_btn}>Reset</button>
+        </form>
+        </div>
+    </div>
+  )
+}
+
+export default SignupPage
