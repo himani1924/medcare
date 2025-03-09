8415093ccfa2779d8decdc79921f594deaf9ad31
Himani <himani1@tothenew.com>
2025-03-09 12:47:53 +0530

    modified navbar to make responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import styles from './styles/navbar.module.css'
-import Image from 'next/image'
-
-
+'use client'
+import styles from "./styles/navbar.module.css";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_links}>
         <div className={styles.app_logo}>
-        <Image
-          src={'logo.svg'}
-          alt='logo'
-          height={40}
-          width={40}
-        />
-        <div className={styles.logo}>MedCare</div>
+          <Image src={"logo.svg"} alt="logo" height={40} width={40} />
+          <div className={styles.logo}>MedCare</div>
         </div>
-      
+
         <ul className={styles.navLinks}>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Appointments</a></li>
-          <li><a href="#">Health Blog</a></li>
-          <li><a href="#">Reviews</a></li>
-    </ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Appointments</a>
+          </li>
+          <li>
+            <a href="#">Health Blog</a>
+          </li>
+          <li>
+            <a href="#">Reviews</a>
+          </li>
+        </ul>
+          {/* mobile navigation */}
+          <div>
+
+          </div>
       </div>
-      
+
       <div className={styles.authButtons}>
         <button className={styles.login}>Login</button>
         <button className={styles.register}>Register</button>
@@ -34,4 +39,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar
+export default Navbar;
