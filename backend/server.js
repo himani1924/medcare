b43262b2da0b2c05399b0ab5a1a40458563f71d8
Himani <himani1@tothenew.com>
2025-03-28 12:19:45 +0530

    file size limit

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000", // Allow frontend
-  credentials: true, // Allow cookies & authentication
+  origin: ["http://localhost:3000", 'http://localhost:3001'],
+  credentials: true,
 }));
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 const PgStore = pgSession(session);
 app.use(session({
