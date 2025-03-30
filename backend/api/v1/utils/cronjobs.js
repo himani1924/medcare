34e658c7825593d13507a18c5abf4d2b18920224
Himani <himani1@tothenew.com>
2025-03-30 15:31:49 +0530

    cron

--- backend/api/v1/utils/cronjobs.js
+++ backend/api/v1/utils/cronjobs.js
@@ -3,5 +3,4 @@ import pool from '../../db/index.js';
 
 cron.schedule('* * * * *', async () => {  
     await pool.query("DELETE FROM otps WHERE expires_at <= NOW()");
-    console.log("Expired OTPs deleted.");
 });
